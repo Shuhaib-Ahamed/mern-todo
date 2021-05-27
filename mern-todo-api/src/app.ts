5acef0551ab184488e0f8c6535e2b3029245a253
Shuhaib-Ahamed <shuhaib.2019678@iit.ac.lk>
2021-05-27 09:27:58 +0530

    Update app.ts

--- mern-todo-api/src/app.ts
+++ mern-todo-api/src/app.ts
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(routes);
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
+  res.send('MERN-TODO-APP!');
 });
 
 mongoose.set('useFindAndModify', true);
